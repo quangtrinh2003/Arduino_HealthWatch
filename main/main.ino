5e7546ea98ae1d50b1cab7703fd875b33ace496c
quangtrinh2003 <phungquangtrinh54@gmail.com>
2022-12-13 20:57:14 +0700

    Update subscreen when press button
    Press button will enable the subscreen (show the IP and date & hour), re-press it will enable the mainscreen

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,30 @@
+#include <ESP8266WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL345_U.h>
 #include <LiquidCrystal_I2C.h>
 #define TCAADDR 0x70
+#define button D5
 
+//Init AXDL345 && LCD1602
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-/* Assign a I2C to this sensor at the same time */
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(0x53);
-float threshold = 120;
+float threshold = 150;
 int steps = 0;
 float distance = 0;
+int choice = 1;
+
+//Init webserver && NTP 
+WiFiUDP ntpUDP;
+const int utcOffsetInSeconds = 7*60*60;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+WiFiServer server(80);
+const char *ssid     = "Thuan";
+const char *password = "0908270035";
+String weekDays[7]={"CN.", "Th2", "Th3", "Th4", "Th5", "Th6", "Th7"};
+String date;
 
 byte shoes[] = {
   B00000,
@@ -58,18 +73,21 @@ void khoiDongLCD()
   lcd.clear();
 }
 
-void tcaselect(uint8_t i) 
-{
-  if (i > 7) return;
- 
-  Wire.beginTransmission(TCAADDR);
-  Wire.write(1 << i);
-  Wire.endTransmission();  
-}
-
 void setup() 
 {
   Serial.begin(9600);
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  server.begin();
+  timeClient.begin();
+
   Serial.println("Accelerometer Test"); Serial.println("");
   /* Initialise the sensor with the I2C address */
   if(!accel.begin(0x53))
@@ -80,27 +98,20 @@ void setup()
 
   /* Set the range to whatever is appropriate for your project */
   accel.setRange(ADXL345_RANGE_16_G);
-
-  tcaselect(2);
+  
+  //start lcd
   lcd.init();
   lcd.backlight();
   khoiDongLCD();
   lcd.createChar(0, shoes);
   lcd.createChar(1, distanceChar);
+  lcd.clear();
+  // set input button
+  pinMode(button, INPUT);
 }
 
-void loop() 
+void mainScreen()
 {
-  sensors_event_t event; 
-  accel.getEvent(&event);
-  float vector = sqrt((event.acceleration.x*event.acceleration.x) * (event.acceleration.y*event.acceleration.y) * (event.acceleration.z*event.acceleration.z));
-  if(vector > threshold)
-  {
-    steps = steps + 1;
-    distance = distance + 0.3;
-  }
-  tcaselect(2);
-
   lcd.setCursor(0,0);
   lcd.write(0);
   lcd.setCursor(1,0);
@@ -113,5 +124,51 @@ void loop()
   lcd.printstr(":");
   lcd.setCursor(2, 1);
   lcd.print(distance);
+}
+
+void subScreen()
+{
+  timeClient.update();
+  lcd.setCursor(4,0);
+  date = weekDays[timeClient.getDay()] + "," + timeClient.getHours() + ":" + timeClient.getMinutes();
+  if(timeClient.getHours() >= 12)
+  {
+    date += "PM";
+  }
+  else
+  {
+    date += "AM";
+  }
+  lcd.print(date);
+  lcd.setCursor(1, 1);
+  lcd.print(WiFi.localIP());
+}
+
+void loop() 
+{
+  sensors_event_t event; 
+  accel.getEvent(&event);
+  float vector = sqrt((event.acceleration.x*event.acceleration.x) * (event.acceleration.y*event.acceleration.y) * (event.acceleration.z*event.acceleration.z));
+  if(vector > threshold)
+  {
+    steps = steps + 1;
+    distance = distance + 0.3;
+  }
+  
+  if(digitalRead(button) == HIGH)
+  {
+    choice += 1;
+  }
+
+  if(choice % 2 == 0)
+  {
+    lcd.clear();
+    subScreen();
+  }
+  else
+  {
+    lcd.clear();
+    mainScreen();
+  }
   delay(1000);
 }
